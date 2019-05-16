5fc213f80ea315397a8799897630ec815eae4e55
Beatriz Câmara <beatrizslcam@gmail.com>
2019-05-16 10:49:05 -0400

    Exercise: WorkingWithMultipleProcesses-4 and 5

--- spawn/spawnLink3.exs
+++ spawn/spawnLink3.exs
@@ -1,7 +1,5 @@
-#Exercise: WorkingWithMultipleProcesses-4
-#Do the same, but have the child raise an exception. What difference do
-#you see in the tracing?
-
+#Exercise: WorkingWithMultipleProcesses-5
+#Repeat the two, changing spawn_link to spawn_monitor .
 
 defmodule SpawnLink do
 
@@ -11,13 +9,13 @@ defmodule SpawnLink do
   def child(pidparent)do
 
     send(pidparent,"oi")
-    raise RuntimeError
-    exit
+    raise RuntimeError # raise an exception
+  #  exit(:boom)        # kill the child
   end
 
   def parent do
     Process.flag(:trap_exit,true)
-    spawn_link(__MODULE__, :child, [self()])
+    spawn_monitor(__MODULE__, :child, [self()])
     sleep 500
 
     receive do
